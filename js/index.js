f5a217b9186b7cb9ea8555ffeeeb31e1ca95dbef
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-09-13 14:17:51 +0200

    Ik heb de snap button werkend gemaakt

--- js/index.js
+++ js/index.js
@@ -1,6 +1,47 @@
 $(document).ready(function () {
-  console.log("joe");
+  function makePath(name) {
+    const basePath = "/styles/images/Characters/";
+    const path = basePath + name;
+
+    return path;
+  }
+
+  function nextImage() {
+    if (window.index == maxLength) {
+      window.index = 0;
+    }
+
+    let nextImageSrc = avengerImages[window.index];
+    $("#character").attr("src", nextImageSrc);
+
+    window.index++;
+  }
+
+  const avengerImages = [
+    makePath("spiderman.png"),
+    makePath("black-widow.png"),
+    makePath("captain-america.png"),
+    makePath("hawkeye.png"),
+    makePath("scarlet-witch.png"),
+    makePath("smart-hulk.png"),
+    makePath("thor.png"),
+  ];
+
+  const maxLength = avengerImages.length;
+  window.index = 0;
+
+  nextImage();
+
   $(".dust-btn").click(function () {
-    $(this).toggleClass("clicked");
+    $(".dust-btn").addClass("clicked");
+    $("#character").addClass("snap-img");
+
+    setTimeout(function () {
+      $(".dust-btn").removeClass("clicked");
+      $("#character").removeClass("snap-img");
+      $("#character").attr("src", "");
+
+      nextImage();
+    }, 1900);
   });
 });
